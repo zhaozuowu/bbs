52f3f56bec33309c77c3df9b58fc2e5ef106ef58
zhaozuowu <gaofandi1236677@126.com>
2018-05-26 14:56:00 +0000

    filing css styles

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -6,8 +6,25 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title','Sample') - Laravel 新手教程 </title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+<header class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            <a href="/" id="logo">Sample App</a>
+            <nav>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="/help">帮助</a></li>
+                    <li><a href="#">登录</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+<div class="container">
+    @yield('content')
+</div>
+<script type="text/javascript" src="/js/app.js"></script>
 </body>
 </html>
